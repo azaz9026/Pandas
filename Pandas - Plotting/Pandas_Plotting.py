d2068e8b4ee8e0fe19c0d030cd42cda7b3c51e83
Md Azaz <mdazaz9026@gmail.com>
2024-01-12 22:15:17 +0530

    # Pandas - Plotting

---
+++ Pandas - Plotting/Pandas_Plotting.py
@@ -0,0 +1,110 @@
+# Pandas - Plotting ----------------------------------------------------------------------------------------------------------------------------------------
+
+'''
+Plotting --------------------------------------------------------------------------------------------------------------------------------------------------
+
+Pandas uses the plot() method to create diagrams.
+
+We can use Pyplot, a submodule of the Matplotlib library to visualize the diagram on the screen.
+'''
+# Import pyplot from Matplotlib and visualize our DataFrame:
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data.csv')
+
+df.plot()
+
+plt.show()
+
+# ----------------------------------------------------------------------------------------------------------------------------------
+
+#Three lines to make our compiler able to draw:
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data.csv')
+
+df.plot()
+
+plt.show()
+
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
+
+
+
+'''
+Scatter Plot --------------------------------------------------------------------------------------------------------------------------------------
+
+Specify that you want a scatter plot with the kind argument:
+
+kind = 'scatter'
+
+A scatter plot needs an x- and a y-axis.
+
+In the example below we will use "Duration" for the x-axis and "Calories" for the y-axis.
+
+Include the x and y arguments like this:
+
+x = 'Duration', y = 'Calories'
+'''
+
+
+#Three lines to make our compiler able to draw:
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data.csv')
+
+df.plot(kind = 'scatter', x = 'Duration', y = 'Calories')
+
+plt.show()
+
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
+
+
+
+'''
+Histogram ----------------------------------------------------------------------------------------------------------------------------------------
+
+Use the kind argument to specify that you want a histogram:
+
+kind = 'hist'
+
+A histogram needs only one column.
+
+A histogram shows us the frequency of each interval, e.g. how many workouts lasted between 50 and 60 minutes?
+
+In the example below we will use the "Duration" column to create the histogram:
+'''
+
+#Three lines to make our compiler able to draw:
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data.csv')
+
+df["Duration"].plot(kind = 'hist')
+
+plt.show()
+
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
