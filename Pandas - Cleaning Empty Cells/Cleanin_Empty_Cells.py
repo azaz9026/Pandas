df417c7f5c4f374fa655d698365b707167a9c421
Md Azaz <125148564+azaz9026@users.noreply.github.com>
2024-01-16 23:45:33 +0530

    Update Cleanin_Empty_Cells.py

--- Pandas - Cleaning Empty Cells/Cleanin_Empty_Cells.py
+++ Pandas - Cleaning Empty Cells/Cleanin_Empty_Cells.py
@@ -12,7 +12,7 @@ One way to deal with empty cells is to remove rows that contain empty cells.
 This is usually OK, since data sets can be very big, and removing a few rows will not have a big impact on the result.
 '''
 
-import pandas as pd
+import pandas as pD
 
 df = pd.read_csv('data1.csv')
 
